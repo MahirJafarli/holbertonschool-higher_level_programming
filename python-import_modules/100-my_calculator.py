53f5ab6e90fdb97894f0e9332e4e31dc8aee8dfe
MahirJafarli <jafarlimahir@gmail.com>
2026-04-03 18:51:54 +0000

    Fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
