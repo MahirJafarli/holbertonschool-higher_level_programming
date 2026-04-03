7e6c43732db37d8f2a238efdaf4f865cee554718
MahirJafarli <jafarlimahir@gmail.com>
2026-04-03 19:18:14 +0000

    Fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 
 def main():
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
     ops = {
         "+": add,
@@ -21,7 +21,7 @@ def main():
 
     if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     result = ops[operator](a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
