75927b23b79c7756296e734307b051685c86898c
MahirJafarli <jafarlimahir@gmail.com>
2026-04-09 07:42:50 +0000

    Task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
+    results = []
+    for num in my_list:
+        if num % 2 == 0:
+            results.append(True)
+        else:
+            results.append(False)
+
+    return results
