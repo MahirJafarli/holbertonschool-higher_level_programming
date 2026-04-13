ec0ccb5eb657dfe3582abe489b63e1877792e5e5
MahirJafarli <jafarlimahir@gmail.com>
2026-04-13 14:06:52 +0000

    Create Task 6

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError exception with a specific message.
+
+    Args:
+        message: The string message to be associated with the exception.
+    """
+    raise NameError(message)
