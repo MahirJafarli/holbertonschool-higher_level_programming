975cdb1859d4a1ef5bd7f66706d04b99c23849d3
MahirJafarli <jafarlimahir@gmail.com>
2026-04-13 13:47:53 +0000

    Fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,4 @@
 def safe_print_list(my_list=[], x=0):
-    """
-    Prints x elements of a list.
-    
-    Args:
-        my_list: The list to print from.
-        x: The number of elements to print.
-        
-    Returns:
-        The real number of elements printed.
-    """
     count = 0
     for i in range(x):
         try:
