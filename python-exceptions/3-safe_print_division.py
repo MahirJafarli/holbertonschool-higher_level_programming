2a4708898ceb56f798d9d248c85e1f2e4979c482
MahirJafarli <jafarlimahir@gmail.com>
2026-04-13 13:58:46 +0000

    Create Task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    Divides 2 integers and prints the result.
+
+    Args:
+        a: The numerator.
+        b: The denominator.
+
+    Returns:
+        The result of the division, otherwise None.
+    """
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+    return result
