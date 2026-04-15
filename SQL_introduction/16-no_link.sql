060a891e02995dded87b182042eb4492e4d0d9e0
MahirJafarli <jafarlimahir@gmail.com>
2026-04-15 15:31:15 +0000

    Create Task 16

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table second_table
+-- Filters out rows without a name value
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
