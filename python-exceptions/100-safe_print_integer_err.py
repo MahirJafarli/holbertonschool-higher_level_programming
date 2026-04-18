ad6fcfe1cd8730b223336a13e9294b681ce19a0c
MahirJafarli <jafarlimahir@gmail.com>
2026-04-18 16:23:52 +0000

    Create Task 7

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Prints an integer with "{:d}".format().
+    
+    Args:
+        value: The value to print (can be any type).
+        
+    Returns:
+        True if value was printed correctly as an integer.
+        False otherwise, with an error message printed to stderr.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return False
