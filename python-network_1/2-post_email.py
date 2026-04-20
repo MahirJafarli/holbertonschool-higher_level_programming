1eb3bd920159218ef3719c6ac2fcd8749c5c81c3
MahirJafarli <jafarlimahir@gmail.com>
2026-04-20 19:51:24 +0000

    Fix

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,15 +7,12 @@ the body of the response (decoded in utf-8).
 import urllib.parse
 import urllib.request
 import sys
-
-
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {'email': email}
     data = urllib.parse.urlencode(values).encode('ascii')
     req = urllib.request.Request(url, data=data, headers={'cf-clearance': 'true'})
-
     with urllib.request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
