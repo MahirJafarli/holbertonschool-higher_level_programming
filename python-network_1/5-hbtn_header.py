63f0c23e0a09b84b78967ccce4a5181042c14c37
MahirJafarli <jafarlimahir@gmail.com>
2026-04-20 20:17:41 +0000

    Fix

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url, headers={'cf-clearance': 'true'})
-    
+
     request_id = r.headers.get('X-Request-Id')
 
     print(request_id)
