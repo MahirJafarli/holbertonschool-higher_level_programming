73d8974f5263b9bea021f052b1b6651436cae720
MahirJafarli <jafarlimahir@gmail.com>
2026-04-20 19:54:27 +0000

    Fix

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 A script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays
-the body of the response (decoded in utf-8).
+the body of the response.
 """
 import urllib.parse
 import urllib.request
