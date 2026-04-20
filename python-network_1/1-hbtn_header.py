656d6ab6e1c55bed92ba27e51724403c6ece528f
MahirJafarli <jafarlimahir@gmail.com>
2026-04-20 19:26:45 +0000

    Create Task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL, sends a request to the URL, and displays
+the value of the X-Request-Id variable found in the header of the response.
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url, headers={'cf-clearance': 'true'})
+
+    with urllib.request.urlopen(req) as response:
+        request_id = response.headers.get('X-Request-Id')
+        print(request_id)
